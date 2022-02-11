baf803f93f4f08d339cf9e4cf1908b8c5effa327
VinayakMarv <vinayakmarv@gmail.com>
2022-02-11 08:38:49 -0800

    Player Movement

--- Air Hokey/Assets/PlayerMovement.cs
+++ Air Hokey/Assets/PlayerMovement.cs
@@ -1,15 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    void Update()
-    {
-        if (Input.touchCount>0 && Input.GetTouch(0).phase==TouchPhase.Moved){
-            Vector2 pos;
-            pos=Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x,Input.mousePosition.y));
-            transform.position=new Vector2(pos.x,pos.y+1f);
-        }        
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        //startingPosition = rb.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if(mousePos.x<2.2 && mousePos.x>-2.2 && mousePos.y<-0.5 && mousePos.y>-4.5){
+            //Vector2 clampedMousePos = new Vector2(Mathf.Clamp(mousePos.x,-0.062f,0.062f),Mathf.Clamp(mousePos.y,-0.044f,0.044f));
+            rb.position=mousePos;
+            }
+        }
     }
 }
